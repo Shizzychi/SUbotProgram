97903c7b32e62e5d986b05c18931b5fbce9df066
Shizza <retroman647@gmail.com>
2025-01-02 14:09:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import os
 import json
 from telethon import TelegramClient, events
 import re
-import requests
-from io import BytesIO
-import threading
-import signal
-import sys
 import asyncio
+import sys
 
 config_file = "config.json"
+
 
 def get_config():
     if os.path.exists(config_file):
@@ -33,6 +30,7 @@ def get_config():
 
         return config
 
+
 config = get_config()
 api_id = config["api_id"]
 api_hash = config["api_hash"]
@@ -40,9 +38,14 @@ your_user_id = int(config["user_id"])
 
 client = TelegramClient("userbot_session", api_id, api_hash)
 
+
 def is_math_expression(message):
-    pattern = r"^[\d+\-*/(). ]+$"
-    return re.match(pattern, message)
+    try:
+        eval(message)
+        return True
+    except:
+        return False
+
 
 @client.on(events.NewMessage)
 async def handle_message(event):
@@ -56,20 +59,23 @@ async def handle_message(event):
             except Exception:
                 await event.reply("Ошибка при вычислении.")
 
+
 async def start_bot():
     try:
+        print("Попытка запуска бота...")
         await client.start()
-        print("Бот запущен!")
+        print("Бот успешно запущен!")
         await client.run_until_disconnected()
     except Exception as e:
         print(f"Ошибка запуска: {e}")
         input("Нажмите Enter для возврата в меню...")
 
+
 def stop_bot():
     print("Остановка бота...")
-    loop = asyncio.get_event_loop()
-    loop.stop()
+    asyncio.create_task(client.disconnect())
     print("Бот остановлен.")
+
 
 def menu():
     while True:
@@ -82,12 +88,32 @@ def menu():
   /  /_\  \   |      /     |  |  |  |  /  /_\  \   |  |     |  |   /  /_\  \   
  /  _____  \  |  |\  \----.|  '--'  | /  _____  \  |  `----.|  |  /  _____  \  
 /__/     \__\ | _| `._____||_______/ /__/     \__\ |_______||__| /__/     \__\ 
-                                                                               
+                     version: 0w2.1b                                                          
         """)
         print("\033[0m")
         print("1. Запустить бота")
         print("2. Остановить бота")
         print("3. Удалить сессию (сменить пользователя)")
         print("4. Выйти")
-        
+
         choice = input("\nВыберите опцию: ")
+
+        if choice == "1":
+            asyncio.run(start_bot())
+        elif choice == "2":
+            stop_bot()
+        elif choice == "3":
+            if os.path.exists("userbot_session.session"):
+                os.remove("userbot_session.session")
+                print("Сессия удалена.")
+            else:
+                print("Сессия не найдена.")
+        elif choice == "4":
+            print("Выход из программы.")
+            sys.exit()
+        else:
+            print("Неверный ввод. Попробуйте снова.")
+
+
+if __name__ == "__main__":
+    menu()
